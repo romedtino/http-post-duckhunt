35e114db66ccf92f9e6f31ef54ec991216e65973
Jerome Tolentino <romedtino@gmail.com>
2019-03-04 20:10:57 -0800

    Updated tests to listen for promises

--- examples/lowdb_example/test/test.js
+++ examples/lowdb_example/test/test.js
@@ -32,95 +32,54 @@ setDefaultRecv = function(duckhunt) {
 describe('Duckhunt Test', function () {
   describe('startDuckHunt-valid', function() {
     
-    it('should start the duck hunt game correctly', function (done) {
+    it('should start the duck hunt game correctly', function () {
       
       duckhunt.startDuckHunt(defaultRecv, duckDB);
       expect(duckhunt.isRunning()).to.be.true;
       stopHuntTest();
-      done();
     });
     
-    it('should try to befriend an entity', function (done) {
-      let onFriendRecv = function(duckResults) {
-        let friendKeys = duckhunt.__get__('miss_friend');
-        friendKeys.push("You're friends with");
-        
-        console.log("onFriendRecv RECEIVER: [uniqueID]-" + duckResults.uniqueID);
-        console.log("onFriendRecv RECEIVER: [success]-" + duckResults.success);
-        console.log("onFriendRecv RECEIVER: [duckMessage]-" + duckResults.duckMessage);
-        console.log("onFriendRecv RECEIVER: [chat]-" + duckResults.chat);
-        console.log("onFriendRecv RECEIVER: [userOnly]-" + duckResults.userOnly);
-        
-        expect(friendKeys.filter(entry => duckResults.chat.includes(entry))).to.have.lengthOf(1);
-        stopHuntTest();
-        done();
-      }
-      
-      setDefaultRecv(duckhunt);
+    it('should try to befriend an entity',  async () => {
+      let friendKeys = duckhunt.__get__('miss_friend');
+      friendKeys.push(["You're friends with"]);
       
       let createDuck = duckhunt.__get__('createDuck');
       createDuck();
-      duckhunt.__set__('callback', onFriendRecv);
+      
       let assessHitOrMiss = duckhunt.__get__('assessHitOrMiss')
-      assessHitOrMiss("bruce", "bef", new Date());
+      const data = await assessHitOrMiss("bruce", "bef", new Date());
+      
+      expect(friendKeys.filter(entry => data.chat.includes(entry))).to.have.lengthOf(1);
+      stopHuntTest();
       
     });
     
-    it('should try to kill an entity', function (done) {
-      let onBangRecv = function(duckResults) {
-        let bangKeys = duckhunt.__get__('miss_bang');
-        bangKeys.push("You have shot");
-        
-        console.log("onBangRecv RECEIVER: [uniqueID]-" + duckResults.uniqueID);
-        console.log("onBangRecv RECEIVER: [success]-" + duckResults.success);
-        console.log("onBangRecv RECEIVER: [duckMessage]-" + duckResults.duckMessage);
-        console.log("onBangRecv RECEIVER: [chat]-" + duckResults.chat);
-        console.log("onBangRecv RECEIVER: [userOnly]-" + duckResults.userOnly);
-        
-        expect(bangKeys.filter(entry => duckResults.chat.includes(entry))).to.have.lengthOf(1);
-        stopHuntTest();
-        done();
-      }
-      
-      setDefaultRecv(duckhunt);
+    it('should try to kill an entity', async () => {
+      let  bangKeys = duckhunt.__get__('miss_bang');
+      bangKeys.push(["You have shot"]);
       
       let createDuck = duckhunt.__get__('createDuck');
       createDuck();
-      duckhunt.__set__('callback', onBangRecv);
-      let assessHitOrMiss = duckhunt.__get__('assessHitOrMiss')
-      assessHitOrMiss("bruce", "bang", new Date());
       
+      let assessHitOrMiss = duckhunt.__get__('assessHitOrMiss')
+      const data = await assessHitOrMiss("bruce", "bang", new Date());
+      
+      expect(bangKeys.filter(entry => data.chat.includes(entry))).to.have.lengthOf(1);
+      stopHuntTest(); 
     });
     
-    it('should return a list of killers', function (done) {
-      let onListRecv = function(duckResults) {
-        
-        console.log("onListRecv RECEIVER: [uniqueID]-" + duckResults.uniqueID);
-        console.log("onListRecv RECEIVER: [success]-" + duckResults.success);
-        console.log("onListRecv RECEIVER: [duckMessage]-" + duckResults.duckMessage);
-        console.log("onListRecv RECEIVER: [chat]-" + duckResults.chat);
-        console.log("onListRecv RECEIVER: [userOnly]-" + duckResults.userOnly);
-        
-        expect(duckResults.userOnly.includes('Here are people who hate dem ducks')).to.be.true;
-        done();
-      }
-      duckhunt.__set__('callback', onListRecv);
-      duckhunt.lookUpKills("");
+    it('should return a list of killers', async () => {
+      
+      const data = await duckhunt.lookUpKills("");
+      expect(data.userOnly.includes('Here are people who hate dem ducks')).to.be.true;
     });
-    it('should return a list of friends', function (done) {
-      let onListRecv = function(duckResults) {
-        console.log("onListRecv RECEIVER: [uniqueID]-" + duckResults.uniqueID);
-        console.log("onListRecv RECEIVER: [success]-" + duckResults.success);
-        console.log("onListRecv RECEIVER: [duckMessage]-" + duckResults.duckMessage);
-        console.log("onListRecv RECEIVER: [chat]-" + duckResults.chat);
-        console.log("onListRecv RECEIVER: [userOnly]-" + duckResults.userOnly);
-        
-        expect(duckResults.userOnly.includes('Here are people with duck faced friends')).to.be.true;
-        expect(duckResults.userOnly.includes(',')).to.be.false;
-        done();
-      }
-      duckhunt.__set__('callback', onListRecv);
-      duckhunt.lookUpFriends("bruce");
+    
+    it('should return a list of friends', async () => {
+
+      const data = await duckhunt.lookUpFriends("bruce");
+      expect(data.userOnly.includes('Here are people with duck faced friends')).to.be.true;
+      expect(data.userOnly.includes(',')).to.be.false;
+
     });
     
   });
